a334fb96021c541bffcbc93b9cf0faea1855205a
Gabriel Lima <gabriellima01.js@gmail.com>
2023-05-30 17:12:00 -0300

    new classes

---
+++ 07-aula/arithmeticOperators.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main(){
+    // arithmetic operators
+
+    // + (addition)
+    // - (subtraction)
+    // * (multiplication)
+    // / (division)
+    // % (modulus)
+    // ++ increment
+    // -- decrement
+
+    int x = 5;
+    int y = 2;
+
+    float z = x / (float) y;
+
+    printf("%.1f", z);
+
+    return 0;
+}
---
+++ 08-aula/assignmentOperators.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(){
+    // augmented assignment operators = used to replace a statement where an operato
+    // take a variable as one of its arguments
+    // and then assings the result back to the same variable
+
+
+    int x = 10;
+
+
+    x = x + 2;
+    x+= 2;
+
+}
